de60da89029205b183da03c34405907c47882b42
Arjun Mehta <arjunmeht@gmail.com>
2016-11-18 13:33:49 -0800

    Updates test for unreferencing without a secondary keepalive

--- test/spawn.js
+++ test/spawn.js
@@ -1,6 +1,16 @@
 var heartbeats = require('../main');
 var heart = heartbeats.createHeart(1000);
 
-setTimeout(function() {}, 2000);
+heart.createEvent(1, {repeat: 3}, function() {
+    console.log('testing unref... 1 repeat 3');
+});
 
-// should exit after 2 seconds
+heart.createEvent(2, {repeat: 2}, function() {
+    console.log('testing unref... 2 repeat 2');
+});
+
+heart.createEvent(3, {repeat: 1}, function() {
+    console.log('testing unref... 3 repeat 1');
+});
+
+// should exit after 4 seconds
